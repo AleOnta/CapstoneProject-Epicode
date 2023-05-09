93b45b9198edc5c007f68db9855b0635b582ff1c
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-09 23:02:37 +0200

    adds primary custom exception handler

---
+++ CinemaCapstoneBE/src/main/java/com/capstone/main/exception/CustomExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.capstone.main.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	ResponseEntity<String> manageEntityNotFoundException(EntityNotFoundException e) {
+		return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(EntityExistsException.class)
+	ResponseEntity<String> manageEntityExistsException(EntityExistsException e) {
+		return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+	}
+	
+	@ExceptionHandler(NullPointerException.class)
+	ResponseEntity<String> manageNullPointerException(NullPointerException e) {
+		return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+	
+}
