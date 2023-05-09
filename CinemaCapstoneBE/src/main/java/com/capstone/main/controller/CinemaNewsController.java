8dd4f603afbba870775894ed1ac41e44bd733ad9
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-09 23:01:12 +0200

    adds news & room controller classes

---
+++ CinemaCapstoneBE/src/main/java/com/capstone/main/controller/CinemaNewsController.java
@@ -0,0 +1,65 @@
+package com.capstone.main.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.capstone.main.model.CinemaNews;
+import com.capstone.main.service.CinemaNewsService;
+
+@RestController
+@RequestMapping("/api/news")
+public class CinemaNewsController {
+
+	@Autowired
+	private CinemaNewsService newsService;
+	
+	@GetMapping
+	public ResponseEntity<List<CinemaNews>> findAllCinemaNews() {
+		return new ResponseEntity<List<CinemaNews>>(newsService.findAllCinemaNews(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CinemaNews> findNewsById(@PathVariable Long id) {
+		return new ResponseEntity<CinemaNews>(newsService.findNewsById(id), HttpStatus.OK);
+	}
+	
+	@GetMapping("/title/{title}")
+	public ResponseEntity<CinemaNews> findNewsByTitle(@PathVariable String title) {
+		return new ResponseEntity<CinemaNews>(newsService.findNewsByTitle(title), HttpStatus.OK);
+	}
+	
+	@GetMapping("/author/{author}")
+	public ResponseEntity<List<CinemaNews>> findNewsByAuthor(@PathVariable String author) {
+		return new ResponseEntity<List<CinemaNews>>(newsService.findNewsByAuthor(author), HttpStatus.OK);
+	}
+	
+	@GetMapping("/r_date/{redactDate}")
+	public ResponseEntity<List<CinemaNews>> findNewsByRedactDate(@PathVariable LocalDate redactDate) {
+		return new ResponseEntity<List<CinemaNews>>(newsService.findNewsByRedactDate(redactDate), HttpStatus.OK);
+	}
+	
+	@PostMapping("/{filmId}")
+	public ResponseEntity<String> addNewCinemaNews(@RequestBody CinemaNews news, @PathVariable Long filmId) {
+		return new ResponseEntity<String>(newsService.persistNews(news, filmId), HttpStatus.CREATED);
+	}
+	
+	@PutMapping
+	public ResponseEntity<String> updateCinemaNews(@RequestBody CinemaNews news) {
+		return new ResponseEntity<String>(newsService.updateNews(news), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteNewsById(@PathVariable Long id) {
+		return new ResponseEntity<String>(newsService.deleteNewsById(id), HttpStatus.ACCEPTED);
+	}
+}
---
+++ CinemaCapstoneBE/src/main/java/com/capstone/main/controller/CinemaRoomController.java
@@ -0,0 +1,53 @@
+package com.capstone.main.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.capstone.main.model.CinemaRoom;
+import com.capstone.main.service.CinemaRoomService;
+
+@RestController
+@RequestMapping("/api/rooms")
+public class CinemaRoomController {
+
+	@Autowired CinemaRoomService roomService;
+	
+	@GetMapping
+	public ResponseEntity<List<CinemaRoom>> findAllCinemaRoom() {
+		return new ResponseEntity<List<CinemaRoom>>(roomService.findAllRooms(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CinemaRoom> findCinemaRoomById(@PathVariable Long id) {
+		return new ResponseEntity<CinemaRoom>(roomService.findRoomById(id), HttpStatus.OK);
+	}
+	
+	@GetMapping("/name/{name}")
+	public ResponseEntity<CinemaRoom> findCinemaRoomByName(@PathVariable String name) {
+		return new ResponseEntity<CinemaRoom>(roomService.findRoomByName(name), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> addNewCinemaRoom(@RequestBody CinemaRoom room) {
+		return new ResponseEntity<String>(roomService.persistRoom(room), HttpStatus.CREATED);
+	}
+	
+	@PutMapping
+	public ResponseEntity<String> updateCinemaRoom(@RequestBody CinemaRoom room) {
+		return new ResponseEntity<String>(roomService.updateRoom(room), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteCinemaRoomById(@PathVariable Long id) {
+		return new ResponseEntity<String>(roomService.deleteRoomById(id), HttpStatus.ACCEPTED);
+	}
+}
