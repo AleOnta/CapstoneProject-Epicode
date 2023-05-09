ee46c0fd4aea1f63b6875d0b52c32328c2818b2a
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-09 22:57:55 +0200

    adds movie & user service classes

---
+++ CinemaCapstoneBE/src/main/java/com/capstone/main/service/CinemaMovieService.java
@@ -0,0 +1,152 @@
+package com.capstone.main.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.capstone.main.model.CinemaMovie;
+import com.capstone.main.repository.CinemaMovieRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class CinemaMovieService {
+
+	@Autowired 
+	private CinemaMovieRepository movieRepository;
+	
+	private void movieTmdbIdExists(Long tmdbId) {
+		
+		log.warn("Movie with tmdbId: {" + tmdbId + "} already exists on database.");
+		throw new EntityExistsException("Movie with tmdbId: {" + tmdbId + "} already exists on database.");
+	}
+	
+	private void movieTitleExists(String title) {
+		
+		log.warn("Movie with title: {" + title + "} already exists on database.");
+		throw new EntityExistsException("Movie with title: {" + title + "} already exists on database.");
+	}
+	
+	private void moviePosterPathExists(String posterPath) {
+		
+		log.warn("Movie with poster path: {" + posterPath + "} already exists on database.");
+		throw new EntityExistsException("Movie with poster path: {" + posterPath + "} already exists on database.");
+	}
+	
+	private void movieCastPathExists(String castPath) {
+		
+		log.warn("Movie with cast path: {" + castPath + "} already exists on database.");
+		throw new EntityExistsException("Movie with cast path: {" + castPath + "} already exists on database.");
+	}
+	
+	public String persistMovie(CinemaMovie movie) {
+		
+		if (movieRepository.existsByTmdbId(movie.getTmdbId())) {
+			movieTmdbIdExists(movie.getTmdbId());
+		} else if (movieRepository.existsByTitle(movie.getTitle())) {
+			movieTitleExists(movie.getTitle());
+		} else if (movieRepository.existsByPosterPath(movie.getPosterPath())) {
+			moviePosterPathExists(movie.getPosterPath());
+		} else if (movieRepository.existsByCastPath(movie.getCastPath())) {
+			movieCastPathExists(movie.getCastPath());
+		}
+		
+		movieRepository.save(movie);
+		log.info("Movie with Title: {" + movie.getTitle() + "} correctly persisted on database.");
+		return "Movie with Title: {" + movie.getTitle() + "} correctly persisted on database.";
+	}
+	
+	public String updateMovie(CinemaMovie movie) {
+		
+		if (!movieRepository.existsById(movie.getId())) {
+			log.warn("Movie with id: {" + movie.getId() + "} doesn't exirsts on database.");
+			log.warn("Movie with id: {" + movie.getId() + "} doesn't exists on database.");
+			throw new EntityNotFoundException("Movie with id: {" + movie.getId() + "} doesn't exists on database.");
+		} else if (movieRepository.existsByTmdbId(movie.getTmdbId())) {
+			CinemaMovie toCheck = findMovieByTmdbId(movie.getTmdbId());
+			if (movie.getId() == toCheck.getId()) {
+				movieTmdbIdExists(movie.getTmdbId());				
+			}
+			movieTmdbIdExists(movie.getTmdbId());
+		} else if (movieRepository.existsByTitle(movie.getTitle())) {
+			CinemaMovie toCheck = findMovieByTitle(movie.getTitle());
+			if (!movie.getId().equals(toCheck.getId())) {		
+				movieTitleExists(movie.getTitle());
+			}
+			movieTitleExists(movie.getTitle());
+		} else if (movieRepository.existsByPosterPath(movie.getPosterPath())) {
+			CinemaMovie toCheck = findMovieByPosterPath(movie.getPosterPath());
+			if (!movie.getId().equals(toCheck.getId())) {
+				moviePosterPathExists(movie.getPosterPath());			
+			}
+			moviePosterPathExists(movie.getPosterPath());
+		} else if (movieRepository.existsByCastPath(movie.getCastPath())) {
+			CinemaMovie toCheck = findMovieByCastPath(movie.getCastPath());
+			if (!movie.getId().equals(toCheck.getId())) {
+				movieCastPathExists(movie.getPosterPath());			
+			}
+		}
+		
+		movieRepository.save(movie);
+		log.info("Movie with Title: {" + movie.getTitle() + "} correctly updated on database.");
+		return "Movie with Title: {" + movie.getTitle() + "} correctly updated on database.";
+	}
+	
+	public String deleteMovieById(Long id) {
+		if (!movieRepository.existsById(id)) {
+			log.warn("Movie with id: {" + id + "} doesn't exists on database.");
+			throw new EntityNotFoundException("Movie with id: {" + id + "} doesn't exists on database.");
+		}
+		
+		movieRepository.deleteById(id);
+		log.info("Movie with id: {" + id + "} correctly removed from database.");
+		return "Movie with id: {" + id + "} correctly removed from database.";
+	}
+	
+	public CinemaMovie findMovieById(Long id) {
+		return movieRepository.findById(id).orElseThrow(
+				() -> new EntityNotFoundException("Movie with id: {" + id + "} doesn't exists on database."));
+	}
+	
+	public CinemaMovie findMovieByTmdbId(Long tmdbId) {
+		return movieRepository.findById(tmdbId).orElseThrow(
+				() -> new EntityNotFoundException("Movie with tmdbId: {" + tmdbId + "} doesn't exists on database."));
+	}
+	
+	public CinemaMovie findMovieByTitle(String title) {
+		return movieRepository.findByTitle(title).orElseThrow(
+				() -> new EntityNotFoundException("Movie with title: {" + title + "} doesn't exists on database."));
+	}
+	
+	public CinemaMovie findMovieByPosterPath(String posterPath) {
+		return movieRepository.findByPosterPath(posterPath).orElseThrow(
+				() -> new EntityNotFoundException("Movie with poster path: {" + posterPath + "} doesn't exists on database."));
+	}
+	
+	public CinemaMovie findMovieByCastPath(String castPath) {
+		return movieRepository.findByCastPath(castPath).orElseThrow(
+				() -> new EntityNotFoundException("Movie with cast path: {" + castPath + "} doesn't exists on database."));
+	}
+	
+	public List<CinemaMovie> findAllMovies() {
+		return (List<CinemaMovie>) movieRepository.findAll();
+	}
+	
+	public List<CinemaMovie> findMoviesByGenre(String genre) {
+		return (List<CinemaMovie>) movieRepository.findByGenreLike(genre);
+	}
+	
+	public List<CinemaMovie> findMoviesByProdCompany(String prodCompany) {
+		return (List<CinemaMovie>) movieRepository.findByProdCompany(prodCompany);
+	}
+	
+	public List<CinemaMovie> findMoviesByTitleLike(String title) {
+		return (List<CinemaMovie>) movieRepository.findByTitleLike(title);
+	}
+	
+	public List<CinemaMovie> findMoviesByReleaseDate(LocalDate releaseDate) {
+		return (List<CinemaMovie>) movieRepository.findByReleaseDate(releaseDate);
+	}
+}
---
+++ CinemaCapstoneBE/src/main/java/com/capstone/main/service/CinemaUserService.java
@@ -0,0 +1,130 @@
+package com.capstone.main.service;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.capstone.auth.entity.User;
+import com.capstone.auth.payload.RegisterDto;
+import com.capstone.auth.service.AuthServiceImpl;
+import com.capstone.auth.service.UserService;
+import com.capstone.main.model.CinemaUser;
+import com.capstone.main.repository.CinemaUserRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class CinemaUserService {
+
+	@Autowired 
+	private CinemaUserRepository userRepository;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private AuthServiceImpl authService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	// user email must be unique!
+	private void userEmailExists(String email) {
+		
+		log.warn("Email: {" + email + "} already exists in the system.");
+		throw new EntityExistsException("Email: {" + email + "} already exists in the system.");
+	}
+	
+	// user username must be unique!
+	private void userUsernameExists(String username) {
+		
+		log.warn("Username: {" + username + "} already exists in the system.");
+		throw new EntityExistsException("Username: {" + username + "} already exists in the system.");
+	}
+	
+	private void userIdNotExisting(Long id) {
+		
+		log.warn("User with id: {" + id + "} doesn't exists on Database.");
+		throw new EntityExistsException("User with id: {" + id + "} doesn't exists on database.");
+	}
+	
+	public String persistUser(CinemaUser user) {
+		
+		if (userRepository.existsByEmail(user.getEmail())) {
+			userEmailExists(user.getEmail());
+		} else if (userRepository.existsByUsername(user.getUsername())) {
+			userUsernameExists(user.getUsername());
+		}
+		
+		RegisterDto registerDto = new RegisterDto(
+				user.getFirstname(), 
+				user.getLastname(),
+				user.getUsername(), 
+				user.getEmail(), 
+				user.getPassword(), 
+				null);
+		
+		authService.register(registerDto);
+		
+		User relatedUser = userService.findByUsername(registerDto.getUsername());
+		user.setRelatedUser(relatedUser);
+		user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+		user.setCinemaPoints(0);
+		
+		userRepository.save(user);
+		log.info("User with username: {" + user.getUsername() + "} correctly persisted on database.");
+		return "User with username: {" + user.getUsername() + "} correctly persisted on database.";
+	}
+	
+	public String updateUser(CinemaUser user) {
+		
+		if (!userRepository.existsById(user.getId())) {
+			userIdNotExisting(user.getId());
+		} else if (userRepository.existsByEmail(user.getEmail())) {
+			userEmailExists(user.getEmail());
+		} else if (userRepository.existsByUsername(user.getUsername())) {
+			userUsernameExists(user.getUsername());
+		}
+		
+		userRepository.save(user);
+		log.info("User with username: {" + user.getUsername() + "} correctly updated on database.");
+		return "User with username: {" + user.getUsername() + "} correctly persisted on database.";
+	}
+	
+	public String deleteUserById(Long id) {
+		
+		if (!userRepository.existsById(id)) {
+			userIdNotExisting(id);
+		}
+		
+		userRepository.deleteById(id);
+		log.info("User with id: {" + id + "} correctly removed from database.");
+		return "User with id: {" + id + "} correctly removed from database.";
+	}
+	
+	public CinemaUser findUserById(Long id) {
+		return userRepository.findById(id).orElseThrow(
+				() -> new EntityNotFoundException("User with id: {" + id + "} doesn't exists on database."));
+	}
+	
+	public CinemaUser findUserByEmail(String email) {
+		return userRepository.findByEmail(email).orElseThrow(
+				() -> new EntityNotFoundException("User with email: {" + email + "} doesn't exists on database."));
+	}
+	
+	public CinemaUser findUserByUsername(String username) {
+		return userRepository.findByUsername(username).orElseThrow(
+				() -> new EntityNotFoundException("User with username: {" + username + "} doesn't exists on database."));
+	}
+	
+	public List<CinemaUser> findAllUser() {
+		return (List<CinemaUser>) userRepository.findAll();
+	}
+
+	public List<CinemaUser> findUsersByBirthdate(LocalDate birthdate) {
+		return (List<CinemaUser>) userRepository.findByBirthdate(birthdate);
+	}
+}
