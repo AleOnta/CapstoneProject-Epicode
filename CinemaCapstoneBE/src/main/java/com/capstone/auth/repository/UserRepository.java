157ab4fa5376d6b070f3876afc12a95b50c26528
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-10 19:51:41 +0200

    refactor user repository

--- CinemaCapstoneBE/src/main/java/com/capstone/auth/repository/UserRepository.java
+++ CinemaCapstoneBE/src/main/java/com/capstone/auth/repository/UserRepository.java
@@ -1,12 +1,17 @@
 package com.capstone.auth.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
+import com.capstone.auth.entity.ERole;
 import com.capstone.auth.entity.User;
-
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long>, PagingAndSortingRepository<User, Long>{
 
     Optional<User> findByEmail(String email);
 
@@ -14,7 +19,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
+	List<User> findByBirthdate(LocalDate birthdate);
+	
+	Page<User> findAll(Pageable pageable);
+
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
+	
 }
