eb539578c2af0e82192c25b975d1ee4b1204da73
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-12 14:14:10 +0200

    built new store.ts

--- cinema_capstone_fe/src/app/store.ts
+++ cinema_capstone_fe/src/app/store.ts
@@ -1,17 +1,21 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import movieSlice from "../features/movieSlice";
+import roomSlice from "../features/roomSlice";
+import programSlice from "../features/programSlice";
+import newsSlice from "../features/newsSlice";
+import userSlice from "../features/userSlice";
+import ticketSlice from "../features/ticketSlice";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+    reducer: {
+        movies: movieSlice,
+        rooms: roomSlice,
+        programs: programSlice,
+        news: newsSlice,
+        user: userSlice,
+        tickets: ticketSlice,
+    }
+})
 
-export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppDispatch = typeof store.dispatch;
