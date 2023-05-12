9bcaa041f901f712bc67466e16c317b13a3bab60
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-12 18:10:39 +0200

    app.tsx

--- cinema_capstone_fe/src/App.tsx
+++ cinema_capstone_fe/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import { NavbarComponent } from "./components/navbar/NavbarComponent"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+    <Container fluid className='p-0'>
+      <NavbarComponent/>
+    </Container>
+    </BrowserRouter>
   );
 }
 
