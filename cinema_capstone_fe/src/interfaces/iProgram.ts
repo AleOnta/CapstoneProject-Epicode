652ee9f4ee529bf7a88caae4adcc541404075d9d
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-13 18:00:34 +0200

    adds Room and Program interfaces

---
+++ cinema_capstone_fe/src/interfaces/iProgram.ts
@@ -0,0 +1,29 @@
+import { IMovie } from "./iMovies";
+
+export interface IProgramMovie {
+  id: number;
+  tmdbId: number;
+  title: string;
+  genre: string;
+  prodCompany: string;
+  filmLength: number;
+  posterPath: string;
+  castPath: string;
+}
+
+export interface IProgramRoom {
+  id: number;
+  name: string;
+  totalSeats: number;
+  timetables: string;
+}
+
+export interface IProgram {
+  id: number;
+  fromDate: Date;
+  toDate: Date;
+  status: string;
+  price: number;
+  movie: IProgramMovie;
+  room: IProgramRoom;
+}
