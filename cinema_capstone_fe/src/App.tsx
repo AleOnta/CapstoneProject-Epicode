ab08999e2b93e565a959a30ea1dc07618c43dbf9
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-13 14:22:54 +0200

    adds react router dom and routes to app.tsx

--- cinema_capstone_fe/src/App.tsx
+++ cinema_capstone_fe/src/App.tsx
@@ -1,15 +1,19 @@
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
-import { NavbarComponent } from "./components/navbar/NavbarComponent"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { NavbarComponent } from "./components/navbar/NavbarComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomepageComponent } from "./components/homepage/HomepageComponent";
 
 function App() {
   return (
     <BrowserRouter>
-    <Container fluid className='p-0'>
-      <NavbarComponent/>
-    </Container>
+      <Container fluid className="p-0">
+        <NavbarComponent />
+        <Routes>
+          <Route path="/home" element={<HomepageComponent />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
