f2f83f6257c7715b547600e1bb765c49bb46f4ee
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-13 14:22:26 +0200

    refactor of navbar and add conditional to display user card on top right corner

--- cinema_capstone_fe/src/components/navbar/NavbarComponent.tsx
+++ cinema_capstone_fe/src/components/navbar/NavbarComponent.tsx
@@ -1,21 +1,22 @@
-import "./Navbar.scss"
-import { Navbar, Container, Row } from "react-bootstrap"
-import { UserCardComponent } from "./UserCardComponent"
-import { SearchbarComponent } from "./SearchbarComponent"
-import { LogoComponent } from "./LogoComponent"
+import "./Navbar.scss";
+import { Navbar, Container, Row } from "react-bootstrap";
+import { UserCardComponent } from "./UserCardComponent";
+import { SearchbarComponent } from "./SearchbarComponent";
+import { LogoComponent } from "./LogoComponent";
+import { useEffect } from "react";
 
 export const NavbarComponent = () => {
+  useEffect(() => {}, []);
 
-    return (
-        <Navbar className="globalNavbar">
-        <Container className="d-flex justify-content-center">
-          <Row className="w-100 align-items-center justify-content-between">
-              <LogoComponent/>
-              <SearchbarComponent/>
-              <UserCardComponent/>
-          </Row>
-        </Container>
-      </Navbar>
-    )
-
-}
+  return (
+    <Navbar className="globalNavbar">
+      <Container className="d-flex justify-content-center">
+        <Row className="w-100 align-items-center justify-content-between">
+          <LogoComponent />
+          <SearchbarComponent />
+          <UserCardComponent />
+        </Row>
+      </Container>
+    </Navbar>
+  );
+};
