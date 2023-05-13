be085c8ba05362fcd75e4f209f0a6e27f86d335e
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-13 18:02:19 +0200

    brought useEffect with dispatch of fetch functions in homepage component

--- cinema_capstone_fe/src/components/homepage/HomepageComponent.tsx
+++ cinema_capstone_fe/src/components/homepage/HomepageComponent.tsx
@@ -1,8 +1,19 @@
 import { Col, Row } from "react-bootstrap";
 import { SwiperComponent } from "./SwiperComponent";
 import { HomepageMainComponent } from "./HomepageMainComponent";
-
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
+import { fetchMovies } from "../../features/movieSlice";
+import { fetchPrograms } from "../../features/programSlice";
 export const HomepageComponent = () => {
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMovies());
+    dispatch(fetchPrograms());
+  }, []);
+
   return (
     <Row className="m-0">
       <Col xs={12} className="px-2 py-5 d-flex justify-content-center">
--- cinema_capstone_fe/src/components/homepage/SwiperComponent.tsx
+++ cinema_capstone_fe/src/components/homepage/SwiperComponent.tsx
@@ -3,21 +3,13 @@ import "./HomePage.scss";
 import "swiper/scss/autoplay";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import { RootState } from "../../app/store";
-import { fetchMovies } from "../../features/movieSlice";
-import { AppDispatch } from "../../app/store";
 
 export const SwiperComponent = () => {
-  const dispatch: AppDispatch = useDispatch();
   const movieStore = useSelector((state: RootState) => state.movies);
-
-  useEffect(() => {
-    dispatch(fetchMovies());
-  }, []);
 
   return (
     <Swiper
