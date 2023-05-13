854564b5ec56d2f5edc43f2301315ccb7d1ea287
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-13 18:01:37 +0200

    adds async thunk to fetch all programs
    function also filters ONGOING and INCOMING programs and set them all in redux store

--- cinema_capstone_fe/src/features/programSlice.ts
+++ cinema_capstone_fe/src/features/programSlice.ts
@@ -1,15 +1,62 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { IProgram } from "../interfaces/iProgram";
 
-const initialState = {
+interface ProgramState {
+  allprograms: IProgram[];
+  onGoing: IProgram[];
+  incoming: IProgram[];
+  status: string;
+}
+
+const initialState: ProgramState = {
   allprograms: [],
   onGoing: [],
   incoming: [],
+  status: "idle",
 };
+
+const buildStateObject = (data: IProgram[]) => {
+  const allData = data;
+  const onGoingData = data.filter((p) => p.status === "ON_GOING");
+  const incomingData = data.filter((p) => p.status === "INCOMING");
+  return {
+    allPrograms: allData,
+    onGoing: onGoingData,
+    incoming: incomingData,
+  };
+};
+
+export const fetchPrograms = createAsyncThunk(
+  "programs/fetch",
+  async (thunkApi) => {
+    try {
+      const response = await fetch("http://localhost:8080/api/programs", {
+        method: "GET",
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 export const programsSlice = createSlice({
   name: "programs",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchPrograms.fulfilled, (state, action) => {
+      const toStore = buildStateObject(action.payload);
+      state.allprograms = toStore.allPrograms;
+      state.onGoing = toStore.onGoing;
+      state.incoming = toStore.incoming;
+      state.status = "fulfilled";
+    });
+  },
 });
 
 export default programsSlice.reducer;
