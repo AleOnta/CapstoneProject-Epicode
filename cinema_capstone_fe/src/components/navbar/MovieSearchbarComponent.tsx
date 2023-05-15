94ca9a9a72cb4c8b7a77b828fbfa685179b4af85
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-15 19:13:14 +0200

    adds feature movie search from searchbar in navbar

---
+++ cinema_capstone_fe/src/components/navbar/MovieSearchbarComponent.tsx
@@ -0,0 +1,40 @@
+import { Card } from "react-bootstrap";
+import { IMovieProps } from "../../interfaces/CommonInterfaces";
+import { BsFillPeopleFill, BsFillStarFill } from "react-icons/bs";
+
+export const MovieSearchBarComponent = ({ movie }: IMovieProps) => {
+  const getGenres = (genres: string) => {
+    let toReturn = "";
+    const genresArray = genres.split("|");
+    genresArray.forEach((genre) => (toReturn += genre + " - "));
+    return toReturn.substring(0, toReturn.length - 2);
+  };
+
+  return (
+    <Card className="d-flex flex-row align-items-center w-100">
+      <Card.Header className="p-0">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.posterPath}`}
+          alt="movie poster"
+          className="movie-img"
+        />
+      </Card.Header>
+      <Card.Body className="py-1 d-flex align-items-center justify-content-between">
+        <span>
+          <Card.Title>{movie.title}</Card.Title>
+          <Card.Text>{getGenres(movie.genre)}</Card.Text>
+        </span>
+        <span>
+          <Card.Text className="d-flex align-items-center">
+            <BsFillStarFill className="me-2" />
+            <span>{movie.vote}</span>
+          </Card.Text>
+          <Card.Text className="d-flex align-items-center">
+            <BsFillPeopleFill className="me-2" />
+            <span>{movie.popularity}</span>
+          </Card.Text>
+        </span>
+      </Card.Body>
+    </Card>
+  );
+};
--- cinema_capstone_fe/src/components/navbar/SearchbarComponent.tsx
+++ cinema_capstone_fe/src/components/navbar/SearchbarComponent.tsx
@@ -1,9 +1,111 @@
-import { Col } from "react-bootstrap"
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import { IMovie } from "../../interfaces/iMovies";
+import { AiOutlineSearch, AiOutlineCloseCircle } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { useClickOutside } from "react-click-outside-hook";
+import { MovieSearchBarComponent } from "./MovieSearchbarComponent";
 
 export const SearchbarComponent = () => {
-    return (
-        <Col xs={6} className="d-flex justify-content-center navbar-search">
-            <input type="text" placeholder="Search movies by a keyword..." className="rounded-3"/>  
-        </Col>
-    )
-}
+  const moviesStore = useSelector((state: RootState) => state.movies.allMovies);
+
+  const [inputSearch, setInputSearch] = useState("");
+  const [moviesFound, setMoviesFound] = useState<IMovie[]>([]);
+  const [isExpanded, setIsExpanded] = useState<Boolean>(false);
+  const [notFoundReturn, setNotFoundReturn] = useState<Boolean>(false);
+  const [ref, isClickedOutside] = useClickOutside();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputSearch(e.target.value);
+    searchMovie(inputSearch);
+  };
+
+  const expandContainer = () => {
+    setIsExpanded(true);
+  };
+
+  const collapseContainer = () => {
+    setIsExpanded(false);
+    setInputSearch("");
+    setMoviesFound([]);
+  };
+
+  const searchMovie = (userInput: string) => {
+    setMoviesFound([]);
+
+    if (userInput !== "") {
+      const result: IMovie[] = [];
+
+      moviesStore.forEach((movie) => {
+        const check = movie.title
+          .toLowerCase()
+          .search(inputSearch.toLocaleLowerCase());
+        if (check !== -1) {
+          result.push(movie);
+        }
+      });
+      if (result.length === 0) {
+        setNotFoundReturn(true);
+      } else {
+        setMoviesFound(result);
+        console.log(moviesFound);
+      }
+    } else {
+      setMoviesFound([]);
+    }
+  };
+
+  const containerVariants = {
+    expanded: {
+      height: "19.8em",
+    },
+    collapsed: {
+      height: "auto",
+    },
+  };
+
+  useEffect(() => {
+    if (isClickedOutside) {
+      collapseContainer();
+    }
+  }, [isClickedOutside]);
+
+  return (
+    <motion.div
+      className="col-6 rounded search-main-container"
+      animate={isExpanded ? "expanded" : "collapsed"}
+      variants={containerVariants}
+      ref={ref}
+    >
+      <div className=" px-0 d-flex justify-content-center align-items-center rounded searchbar-container">
+        <span>
+          <AiOutlineSearch />
+        </span>
+        <input
+          type="text"
+          placeholder="Search movies by a keyword..."
+          value={inputSearch}
+          onChange={(e) => handleChange(e)}
+          onFocus={expandContainer}
+          className="rounded-3"
+        />
+        <span>
+          <AiOutlineCloseCircle
+            className={`search-close-icon ${isExpanded ? "is-shown" : ""}`}
+            onClick={collapseContainer}
+          />
+        </span>
+      </div>
+      <div className="results-container d-flex flex-column align-items-start">
+        {isExpanded && moviesFound.length > 0 ? (
+          moviesFound.map((movie) => (
+            <MovieSearchBarComponent movie={movie} key={movie.tmdbId} />
+          ))
+        ) : notFoundReturn && isExpanded ? (
+          <p>No movie found searching for "{inputSearch}"</p>
+        ) : null}
+      </div>
+    </motion.div>
+  );
+};
