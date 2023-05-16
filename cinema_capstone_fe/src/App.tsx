108723395c797d4201fcaafa298662f369591751
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-16 15:51:53 +0200

    initialized new route as Programs page

--- cinema_capstone_fe/src/App.tsx
+++ cinema_capstone_fe/src/App.tsx
@@ -4,14 +4,16 @@ import { Container } from "react-bootstrap";
 import { NavbarComponent } from "./components/navbar/NavbarComponent";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomepageComponent } from "./components/homepage/HomepageComponent";
+import { ProgramsPageComponent } from "./components/programspage/ProgramsPageComponent";
 
 function App() {
   return (
     <BrowserRouter>
-      <Container fluid className="p-0">
+      <Container fluid className="p-0 app-container">
         <NavbarComponent />
         <Routes>
           <Route path="/home" element={<HomepageComponent />} />
+          <Route path="/programs" element={<ProgramsPageComponent />} />
         </Routes>
       </Container>
     </BrowserRouter>
