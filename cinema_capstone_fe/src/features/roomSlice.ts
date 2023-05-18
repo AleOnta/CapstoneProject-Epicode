c7d5a479ebcd9c318baea63184ed7185d74ba896
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-18 15:00:18 +0200

    adds room fetch middleware

--- cinema_capstone_fe/src/features/roomSlice.ts
+++ cinema_capstone_fe/src/features/roomSlice.ts
@@ -1,13 +1,50 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { IRoom } from "../interfaces/iRoom";
+import axios from "axios";
+import { error } from "console";
 
-const initialState = {
-    allRooms: []
+const roomsURL = "http://localhost:8080/api/rooms";
+
+interface RoomsState {
+  allRooms: IRoom[];
+  status: string;
+  error: string | null;
+}
+
+const initialState: RoomsState = {
+  allRooms: [],
+  status: "idle",
+  error: null,
 };
 
+export const fetchRooms = createAsyncThunk("rooms/fetch", async (thunkAPI) => {
+  try {
+    const response = await axios.get(roomsURL);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return error.message;
+    }
+  }
+});
+
 export const roomsSlice = createSlice({
-    name: "rooms",
-    initialState,
-    reducers: {},
+  name: "rooms",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchRooms.pending, (state) => {
+      state.status = "loading";
+    });
+    builder.addCase(fetchRooms.fulfilled, (state, action) => {
+      state.allRooms = action.payload;
+      state.status = "fulfilled";
+    });
+    builder.addCase(fetchRooms.rejected, (state, action) => {
+      state.status = "failed";
+      state.error = "define how";
+    });
+  },
 });
 
 export default roomsSlice.reducer;
