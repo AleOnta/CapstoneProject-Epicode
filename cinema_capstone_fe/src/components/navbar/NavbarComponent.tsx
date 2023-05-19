924d1bf8194b39b41d923f30b3a37b12183eb0f4
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-19 19:39:20 +0200

    adds useLocation to set d-none on auth path

--- cinema_capstone_fe/src/components/navbar/NavbarComponent.tsx
+++ cinema_capstone_fe/src/components/navbar/NavbarComponent.tsx
@@ -4,12 +4,21 @@ import { UserCardComponent } from "./UserCardComponent";
 import { SearchbarComponent } from "./searchbar/SearchbarComponent";
 import { LogoComponent } from "./LogoComponent";
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 export const NavbarComponent = () => {
+  let location = useLocation();
+  console.log(location);
   useEffect(() => {}, []);
 
   return (
-    <Navbar className="globalNavbar">
+    <Navbar
+      className={`globalNavbar ${
+        (location.pathname === "/auth/login" ||
+          location.pathname === "/auth/register") &&
+        "d-none"
+      }`}
+    >
       <Container className="d-flex justify-content-center">
         <Row className="w-100 align-items-center justify-content-between">
           <LogoComponent />
