9a7ce923701b0d9aa8168db53dc3e8900ee7bfb8
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-19 19:40:28 +0200

    adds userDto interface and minor changes

--- cinema_capstone_fe/src/interfaces/iUser.ts
+++ cinema_capstone_fe/src/interfaces/iUser.ts
@@ -1,9 +1,18 @@
 export interface IUser {
-    id: number,
-    firstname: string,
-    lastname: string,
-    username: string,
-    email: string,
-    cinemaPoints: number,
-    tickets: [] 
-};
+  id: number;
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  cinemaPoints: number;
+  tickets: [];
+}
+
+export interface UserDto {
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  birthdate: Date;
+  password: string;
+}
