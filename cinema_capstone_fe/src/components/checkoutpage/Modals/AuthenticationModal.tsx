37bc41d98a193f96b1b390584b7787d64dd9bf84
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-24 15:05:11 +0200

    refactor of auth modal

--- cinema_capstone_fe/src/components/checkoutpage/Modals/AuthenticationModal.tsx
+++ cinema_capstone_fe/src/components/checkoutpage/Modals/AuthenticationModal.tsx
@@ -7,19 +7,41 @@ export const AuthenticationModal = ({ show, setShow }: ModalProps) => {
 
   return (
     <>
-      <Modal show={show} onHide={() => setShow(false)} className="authModal">
+      <Modal
+        show={show}
+        centered
+        onHide={() => setShow(false)}
+        className="auth-modal"
+      >
         <Modal.Header closeButton>
           <Modal.Title id="example-modal-sizes-title-sm">
-            Proceeding with check-out requires to be registered
+            To proceed with the check-out,<br></br> you need to be logged in
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body className="d-flex aling-items-center justify-content-around py-5">
-          <Button className="px-5" onClick={() => navigate("/auth/register")}>
-            Sign-up
-          </Button>
-          <Button className="px-5" onClick={() => navigate("/auth/login-ch")}>
-            Sign-in
-          </Button>
+        <Modal.Body className="d-flex aling-items-center justify-content-around pt-5 pb-4 mt-2">
+          <div className="d-flex flex-column aling-items-center justify-content-center">
+            <Button
+              variant="outline-light"
+              className="px-5"
+              onClick={() => navigate("/auth/register")}
+            >
+              Sign-up
+            </Button>
+            <p className="desc">Create a new account</p>
+          </div>
+          <div className="d-flex align-items-center">
+            <p className="mb-4 pb-2">OR</p>
+          </div>
+          <div className="d-flex flex-column aling-items-center justify-content-center">
+            <Button
+              variant="outline-secondary"
+              className="px-5"
+              onClick={() => navigate("/auth/login-ch")}
+            >
+              Sign-in
+            </Button>
+            <p className="desc">Login into your account</p>
+          </div>
         </Modal.Body>
       </Modal>
     </>
