be3e3bb26e5c43a4ef30ed9f27bb8469d6a4fe0a
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-24 17:46:52 +0200

    adds spinner for loading

--- cinema_capstone_fe/src/components/homepage/home_swiper/SwiperComponent.tsx
+++ cinema_capstone_fe/src/components/homepage/home_swiper/SwiperComponent.tsx
@@ -13,36 +13,44 @@ export const SwiperComponent = () => {
   const movieStore = useSelector((state: RootState) => state.movies);
 
   return (
-    <Swiper
-      spaceBetween={65}
-      slidesPerView={5}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-      autoplay={{
-        delay: 8000,
-        disableOnInteraction: false,
-      }}
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-      modules={[Autoplay, Pagination, Navigation]}
-      className="mySwiper"
-    >
-      {movieStore.allMovies.map((movie, index) => {
-        return (
-          <Link to={`/movie-focus/${movie.id}`}>
-            <SwiperSlide key={index}>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.posterPath}`}
-                alt="movie poster"
-                className="slider-image"
-              />
-            </SwiperSlide>
-          </Link>
-        );
-      })}
-      ...
-    </Swiper>
+    <>
+      {movieStore.status === "loading" ? (
+        <span className="loader"></span>
+      ) : movieStore.status === "fulfilled" ? (
+        <Swiper
+          spaceBetween={65}
+          slidesPerView={5}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          autoplay={{
+            delay: 8000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {movieStore.allMovies.map((movie, index) => {
+            return (
+              <Link to={`/movie-focus/${movie.id}`}>
+                <SwiperSlide key={index}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.posterPath}`}
+                    alt="movie poster"
+                    className="slider-image"
+                  />
+                </SwiperSlide>
+              </Link>
+            );
+          })}
+          ...
+        </Swiper>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
