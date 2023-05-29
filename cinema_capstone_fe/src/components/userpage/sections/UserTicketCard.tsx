70add92b8136b9e7a0c8597e06602acc3bb56dfc
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-05-29 17:44:44 +0200

    adds qr code on user page tickets at onclick

--- cinema_capstone_fe/src/components/userpage/sections/UserTicketCard.tsx
+++ cinema_capstone_fe/src/components/userpage/sections/UserTicketCard.tsx
@@ -1,16 +1,36 @@
-import { Link } from "react-router-dom";
-import { UserTicketProps } from "../../../interfaces/iUser";
+import { useState } from "react";
 import { format } from "date-fns";
+import QRCode from "react-qr-code";
+import { Link } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
+import { UserTicketProps } from "../../../interfaces/iUser";
 
 export const UserTicketCard = ({ ticket }: UserTicketProps) => {
+  const [clicked, setClicked] = useState<boolean>(false);
+
   return (
-    <Col md={6} className="user-movie-card p-0">
-      <img
-        className="user-movie-img"
-        src={`https://image.tmdb.org/t/p/w500/${ticket.boundFilm.posterPath}`}
-        alt="movie poster"
-      />
+    <Col
+      md={6}
+      className="user-movie-card p-0"
+      onClick={() => setClicked(!clicked)}
+    >
+      {!clicked ? (
+        <img
+          className="user-movie-img"
+          src={`https://image.tmdb.org/t/p/w500/${ticket.boundFilm.posterPath}`}
+          alt="movie poster"
+        />
+      ) : (
+        <div className="user-movie-img qr-container d-flex align-items-center justify-content-center">
+          <QRCode
+            className="user-qr-code"
+            size={175}
+            bgColor="white"
+            fgColor="black"
+            value={`user: TBD - theather: ${ticket.boundRoom.name} - movie: ${ticket.boundFilm.title} - date: ${ticket.perDate} - time: ${ticket.hours} - seats: [${ticket.seatCode}]`}
+          />
+        </div>
+      )}
       <Row className="user-movie-content px-0">
         <Col className="user-movie-content-header px-2 d-flex justify-content-between align-items-center">
           <Link to="">
